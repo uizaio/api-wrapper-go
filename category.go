4cbdf2732e320c9a846ab6798116a8a063b37dac
han.thi.kim.ngoc <han.thi.kim.ngoc@framgia.com>
2019-02-19 10:49:40 +0700

    [#222066][#222069][#222072][#222075][#222078][#222081][#222084] - [Go][Category][Create] [Retrieve] [List] [Update][Delete][Create category relation][Delete category relation] Coding

---
+++ category.go
@@ -0,0 +1,78 @@
+package uiza
+
+type Type string
+
+const (
+	FolderType   Type = "folder"
+	PlaylistType Type = "playlist"
+	TagType      Type = "tag"
+)
+
+type CategoryCreateParams struct {
+	Params      `form:"*"`
+	Name        *string `form:"name"`
+	Type        *Type   `form:"type"`
+	Description *string `form:"description"`
+	OrderNumber *int64  `form:"orderNumber"`
+	Icon        *string `form:"icon"`
+}
+
+type CategoryID struct {
+	ID string `json:"id"`
+}
+
+type CategorySpecData struct {
+	Data *CategorySpec `json:"data"`
+}
+
+type CategorySpec struct {
+	ID          string `json:"id"`
+	Name        string `json:"name"`
+	Type        string `json:"type"`
+	Description string `json:"description"`
+	Slug        string `json:"slug"`
+	OrderNumber int64  `json:"orderNumber"`
+	Icon        string `json:"icon"`
+	Status      int64  `json:"status"`
+	CreatedAt   string `json:"createdAt"`
+	UpdatedAt   string `json:"updatedAt"`
+}
+
+type CategoryIDParams struct {
+	Params `form:"*"`
+	ID     *string `form:"id"`
+}
+
+type CategoryIDData struct {
+	Data *CategoryID `json:"data"`
+}
+
+type CategoryUpdateParams struct {
+	Params      `form:"*"`
+	ID          *string `form:"id"`
+	Name        *string `form:"name"`
+	Type        *Type   `form:"type"`
+	Description *string `form:"description"`
+	OrderNumber *int64  `form:"orderNumber"`
+	Icon        *string `form:"icon"`
+}
+
+type CategoryRelationParams struct {
+	Params      `form:"*"`
+	EntityId    *string   `form:"entityId"`
+	MetadataIds []*string `form:"metadataIds"`
+}
+
+type CategoryRelationData struct {
+	Data []*CategoryRelation `json:"data"`
+}
+
+type CategoryRelation struct {
+	ID         string `json:"id"`
+	EntityId   string `json:"entityId"`
+	MetadataId string `json:"metadataId"`
+}
+
+type CategoryDataList struct {
+	Data []*CategorySpec `json:"data"`
+}
